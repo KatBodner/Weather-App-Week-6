d6fe10a1462e22180f535f94f9f6c2ddf3bf0a08
KatBodner <katrin.bodner@hotmail.de>
2022-06-04 18:14:51 +0200

    updated atmosphere element

--- src/index.js
+++ src/index.js
@@ -44,6 +44,7 @@ function showWeather(response) {
   let temperature = Math.round(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperatureElement");
     temperatureElement.innerHTML = `${temperature}°C`;
+  atmosphereElement.innerHTML = response.data.weather[0].description;
   let iconElement = document.querySelector("#icon");
     iconElement.setAttribute(
     "src",
