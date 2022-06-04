99cfda85be0cb9a86e07bd2ff13c8402bd5eeed4
KatBodner <katrin.bodner@hotmail.de>
2022-06-04 17:32:21 +0200

    update icon with actual data from link

--- src/index.js
+++ src/index.js
@@ -44,4 +44,10 @@ function showWeather(response) {
   let temperature = Math.round(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperatureElement");
     temperatureElement.innerHTML = `${temperature}°C`;
+  let atmosphereElement = document.querySelector("#atmosphere");
+  atmosphereElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  atmosphereElement.setAttribute("alt", response.data.weather[0].description);
 }
